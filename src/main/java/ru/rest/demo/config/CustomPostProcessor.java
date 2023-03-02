d6eceadc8b999af89bef4aa047b0a4671171e4ac
mkhromov <khromovm@clink.tech>
2023-03-02 10:20:52 +0400

    я сдался

---
+++ src/main/java/ru/rest/demo/config/CustomPostProcessor.java
@@ -0,0 +1,45 @@
+package ru.rest.demo.config;
+
+import jakarta.annotation.Priority;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
+import org.springframework.beans.factory.support.RootBeanDefinition;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.PriorityOrdered;
+
+//@Configuration
+//public class CustomPostProcessor implements BeanDefinitionRegistryPostProcessor, PriorityOrdered {
+//    @Override
+//    public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry registry) throws BeansException {
+//        BeanDefinition someConfig = new RootBeanDefinition("ru.rest.demo.config.FlywayConfig");
+//        registry.registerBeanDefinition("flywayConfig", someConfig);
+//    }
+//
+//    @Override
+//    public int getOrder() {
+//        return Ordered.HIGHEST_PRECEDENCE;
+//    }
+//
+//    @Override
+//    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+//
+//    }
+//}
+
+@Configuration
+public class CustomPostProcessor implements BeanDefinitionRegistryPostProcessor {
+    @Override
+    public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry registry) throws BeansException {
+        BeanDefinition someConfig = new RootBeanDefinition("ru.rest.demo.config.FlywayConfig");
+        registry.registerBeanDefinition("flywayConfig", someConfig);
+    }
+
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+
+    }
+}
--- src/main/java/ru/rest/demo/config/FlywayConfig.java
+++ src/main/java/ru/rest/demo/config/FlywayConfig.java
@@ -8,36 +8,31 @@ import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Map;
 
 
-@Configuration
-public class FlywayConfig implements BeanDefinitionRegistryPostProcessor {
-    @Autowired
-    private ApplicationContext context;
+public class FlywayConfig {
 
-    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
-    @Autowired
-    public FlywayConfig(DataSource dataSource) {
-        PasswordEncoder pwdEncoder = (PasswordEncoder) context.getBean("pwdEncoder");
+    private ApplicationContext context;
+    private DataSource dataSource;
+
+    public FlywayConfig(ApplicationContext context, DataSource dataSource) {
+        this.context = context;
+        this.dataSource = dataSource;
+        config();
+    }
+
+    public void config(){
+        PasswordEncoder pwdEncoder = (PasswordEncoder) this.context.getBean("pwdEncoder");
         Flyway.configure()
                 .baselineOnMigrate(true)
-                .dataSource(dataSource)
+                .dataSource(this.dataSource)
                 .locations("classpath:/db/migration","classpath:/db/testdata")
                 .placeholders(Map.of("adminpwd", pwdEncoder.encode("admin")))
                 .load().migrate();
-    }
-
-    @Override
-    public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry registry) throws BeansException {
-
-    }
-
-    @Override
-    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
-
     }
 }
