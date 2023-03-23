48300e88bd40d8419e4f1ea2b8f87968c80ebf6c
mkhromov <khromovm@clink.tech>
2023-03-23 10:48:22 +0400

    CustomPageable little rafactor

--- src/main/java/ru/rest/demo/dto/CustomPageable.java
+++ src/main/java/ru/rest/demo/dto/CustomPageable.java
@@ -1,17 +1,12 @@
 package ru.rest.demo.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class CustomPageable {
     int pageNumber;
     int pageSize;
     long totalElements;
-
-    public CustomPageable(int pageNumber, int pageSize, long totalElements) {
-
-        this.pageNumber = pageNumber;
-        this.pageSize = pageSize;
-        this.totalElements = totalElements;
-    }
 }
